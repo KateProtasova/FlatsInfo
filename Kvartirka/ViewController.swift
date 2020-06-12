80fee701b2722fd33d0124c3b2b87188d6e5c1a1
Kate Protasova <ket9090@gmail.com>
2020-06-12 12:37:40 +0700

    Initial Commit

---
+++ Kvartirka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kvartirka
+//
+//  Created by Екатерина Протасова on 12.06.2020.
+//  Copyright © 2020 Екатерина Протасова. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
